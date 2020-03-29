51927e7ff237e9576105c95b96f0b6685a2e8e93
Suchar230 <dsj230@gmail.com>
2020-03-29 18:04:50 +0200

    top 5 predictions

--- cmdrecogniser.py
+++ cmdrecogniser.py
@@ -6,6 +6,22 @@ import sys
 import os.path
 import imghdr
 
+def top5outputs(outputs):    
+    sortedOutputs = outputs.argsort(descending=True)
+    sortedOutputs = sortedOutputs[:5]    
+    return sortedOutputs
+	
+def top5outputs_labels(outputs, classes):
+    top_5 = top5outputs(outputs)
+    labels = []
+    confidence = []
+    for i in top_5:
+        x = classes[i][10:]
+        p = str(float("{0:.4f}".format(outputs[i]*100))) + "%"
+        labels.append(x)
+        confidence.append(p)
+    return labels, confidence;
+
 if len(sys.argv) > 1:
 	scriptPath = Path(os.path.dirname(os.path.realpath(__file__)))
 
@@ -22,9 +38,15 @@ if len(sys.argv) > 1:
 	if str(imghdr.what(imgPath)) != "None":
 		img = open_image(imgPath)
 		pred_class, pred_idx, outputs = learner.predict(img)
-		print(pred_class) 
+		
+		top5_predictions, top5_confidence = top5outputs_labels(outputs, data.classes)
+
+		for i in range(5):
+			print(top5_confidence[i], top5_predictions[i])
+			
 	else:
 		print("Not image") 
+		
 else:
 	print("No filename") 
 
